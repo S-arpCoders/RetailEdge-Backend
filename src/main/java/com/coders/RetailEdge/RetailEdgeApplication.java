99df864e98f4ddda9aa9303bb0882d3d7e451188
Mulamuleli Mammba <mammba001@gmail.com>
2025-03-04 13:29:25 +0200

    Project Setup

---
+++ src/main/java/com/coders/RetailEdge/RetailEdgeApplication.java
@@ -0,0 +1,13 @@
+package com.coders.RetailEdge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailEdgeApplication.class, args);
+	}
+
+}
